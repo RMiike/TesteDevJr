1ac3802d0e716ce78db935da2edcf6747e18f51f
Renato Miike Alves <rmalves2@stefanini.com>
2020-12-02 22:08:00 -0300

    criacao de entidade basica e entidades do sistema

---
+++ src/dominio/TDJ.Dominio/Entidades/Cliente.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDJ.Dominio
+{
+    class Cliente
+    {
+    }
+}
---
+++ src/dominio/TDJ.Dominio/Entidades/Produto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDJ.Dominio.Entidades
+{
+    class Produto
+    {
+    }
+}
