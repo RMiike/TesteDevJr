fce3a6dde7a3c7916024c3682401092caa5aa4da
Renato Miike Alves <rmalves2@stefanini.com>
2020-12-02 22:39:27 -0300

    adicionado contexto

---
+++ src/repositorio/TDJ.Repositorio/Contexto/TDJDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TDJ.Dominio.Entidades;
+
+namespace TDJ.Repositorio.Contexto
+{
+    public class TDJDbContext : DbContext
+    {
+        public TDJDbContext(DbContextOptions<TDJDbContext> opt) : base(opt) { }
+
+        public DbSet<Cliente> MyProperty { get; set; }
+    }
+}
